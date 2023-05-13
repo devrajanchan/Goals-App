0c036a4ec1b8184e71c6fb0277def141f39c3164
Devraj Anchan <devrajanchan333@gmail.com>
2023-05-14 02:26:16 +0530

    Register,Login,Loout Completed

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,18 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
 import { Link, useNavigate } from 'react-router-dom'
-
+import { useSelector, useDispatch } from 'react-redux'
+import { logout, reset } from '../features/auth/authSlice'
 
 function Header() {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { user } = useSelector((state) => state.auth)
 
+  const onLogout = () => {
+    dispatch(logout())
+    dispatch(reset())
+    navigate('/')
+  }
 
   return (
     <header className='header'>
@@ -11,6 +20,13 @@ function Header() {
         <Link to='/'>GoalSetter</Link>
       </div>
       <ul>
+        {user ? (
+          <li>
+            <button className='btn' onClick={onLogout}>
+              <FaSignOutAlt /> Logout
+            </button>
+          </li>
+        ) : (
           <>
             <li>
               <Link to='/login'>
@@ -23,6 +39,7 @@ function Header() {
               </Link>
             </li>
           </>
+        )}
       </ul>
     </header>
   )
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,21 +1,37 @@
 import { useState, useEffect } from 'react'
-import { FaUser } from 'react-icons/fa'
-
-
+import { FaSignInAlt } from 'react-icons/fa'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { login, reset } from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Login() {
   const [formData, setFormData] = useState({
-    name: '',
     email: '',
     password: '',
-    password2: '',
   })
 
   const { email, password } = formData
 
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
-  
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
 
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+
+    if (isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+  }, [user, isError, isSuccess, message, navigate, dispatch])
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -27,16 +43,23 @@ function Login() {
   const onSubmit = (e) => {
     e.preventDefault()
 
- 
+    const userData = {
+      email,
+      password,
+    }
+
+    dispatch(login(userData))
   }
 
-
+  if (isLoading) {
+    return <Spinner />
+  }
 
   return (
     <>
       <section className='heading'>
         <h1>
-          <FaUser /> Register
+          <FaSignInAlt /> Login
         </h1>
         <p>Login and start setting goals</p>
       </section>
@@ -65,6 +88,7 @@ function Login() {
               onChange={onChange}
             />
           </div>
+
           <div className='form-group'>
             <button type='submit' className='btn btn-block'>
               Submit
